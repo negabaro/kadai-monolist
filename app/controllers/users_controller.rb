06c8917a1329db2e97bae772dcf10e5d1b2d0e7c
negabaro kim <negabaro@gmail.com>
2018-07-08 19:57:16 +0000

    add uniq

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @items = @user.items
+    @items = @user.items.uniq
     @count_want = @user.want_items.count
     
     @count_have = @user.have_items.count
